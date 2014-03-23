f2ff336efe89882dc647b44031d9fbf2595457e4
Kieran <kieran@arcticdesk.com>
2014-03-23 18:23:41 +0000

    Adding PHP uploader

---
+++ src/upload.php
@@ -0,0 +1,72 @@
+<?php
+
+header('Content-Type: text/plain; charset=utf-8');
+
+function get_file_size($size) {
+  $units = array('Bytes', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB');
+  return @round($size / pow(1024, ($i = floor(log($size, 1024)))), 2).' '.$units[$i];
+}
+
+try {
+
+    // Undefined | Multiple Files | $_FILES Corruption Attack
+    // If this request falls under any of them, treat it invalid.
+    if (!isset($_FILES['image']['error'])
+            || is_array($_FILES['image']['error']))
+    {
+        throw new RuntimeException('Invalid parameters.');
+    }
+
+    // Check $_FILES['image']['error'] value.
+    switch ($_FILES['image']['error']) {
+        case UPLOAD_ERR_OK:
+            break;
+        case UPLOAD_ERR_NO_FILE:
+            throw new RuntimeException('No file sent.');
+        case UPLOAD_ERR_INI_SIZE:
+        case UPLOAD_ERR_FORM_SIZE:
+            throw new RuntimeException('Exceeded filesize limit.');
+        default:
+            throw new RuntimeException('Unknown errors.');
+    }
+
+    // You should also check filesize here.
+    if ($_FILES['image']['size'] > 1000000) {
+        throw new RuntimeException('Exceeded filesize limit.');
+    }
+
+    // Check mime type
+    $finfo = new finfo(FILEINFO_MIME_TYPE);
+    if (false === $ext = array_search(
+        $finfo->file($_FILES['image']['tmp_name']),
+        array(
+            'jpg' => 'image/jpeg',
+            'png' => 'image/png',
+            'gif' => 'image/gif',
+        ),
+        true
+    )) {
+        throw new RuntimeException('Invalid file format.');
+    }
+
+    // Create file location
+    $fileLocation = sprintf(
+        './uploads/%s.%s',
+        sha1_file($_FILES['image']['tmp_name']),
+        $ext
+    );
+
+    // Move uploaded file
+    if (!move_uploaded_file($_FILES['image']['tmp_name'], $fileLocation)) {
+        throw new RuntimeException('Failed to move uploaded file.');
+    }
+
+    // Successful upload
+    echo json_encode(array(
+        "URL"   => "{$_SERVER['REQUEST_URI']}/{$fileLocation}",
+        "size"  => get_file_size($_FILES['image']['size'])
+    ));
+
+} catch (RuntimeException $e) {
+    echo $e->getMessage();
+}
